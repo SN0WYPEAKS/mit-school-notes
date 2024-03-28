b28b9b8b7798e8400fc62c19fd82ca7e22e445f0
johnt <john.tucker@ddst.dev>
2024-03-28 15:23:04 -0600

    Updated school notes and reading

--- CS 6.002 Introduction To Computational Thinking And Data Science/fingerExercises/chapter3.1FindPwr.py
+++
@@ -1,6 +0,0 @@
-class findPwr:
-    def getUserInput(userInt):
-        userInput = input("Enter a positive integer: ")
-        while userInt < 0:
-            userInt = userInput
-            print("Oh my bad... I thought I said to enter a POSITIVE integer. Try it again!")
